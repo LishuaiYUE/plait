bc2e97230d0050fabc81bb59ad91acc6fd948068
pubuzhixing8 <pubuzhixing@gmail.com>
2024-11-07 11:54:30 +0800

    fix(core): using ceilToDecimal to set text width to avoid breaking line（safari/firefox `Plait 框架`）

---
+++ .changeset/four-peaches-join.md
@@ -0,0 +1,5 @@
+---
+'@plait/core': patch
+---
+
+using ceilToDecimal to set text width to avoid breaking line（safari/firefox `Plait 框架`）
--- packages/core/src/utils/dom/foreign.ts
+++ packages/core/src/utils/dom/foreign.ts
@@ -5,7 +5,7 @@ export function createForeignObject(x: number, y: number, width: number, height:
     var newForeignObject = document.createElementNS(NS, 'foreignObject');
     newForeignObject.setAttribute('x', `${x}`);
     newForeignObject.setAttribute('y', `${y}`);
-    newForeignObject.setAttribute('width', `${width}`);
+    newForeignObject.setAttribute('width', `${ceilToDecimal(width, 0)}`);
     newForeignObject.setAttribute('height', `${height}`);
     return newForeignObject;
 }
@@ -13,7 +13,7 @@ export function createForeignObject(x: number, y: number, width: number, height:
 export function updateForeignObject(target: SVGForeignObjectElement | SVGGElement, width: number, height: number, x: number, y: number) {
     const foreignObject = target instanceof SVGForeignObjectElement ? target : target.querySelector('foreignObject');
     if (foreignObject) {
-        foreignObject.setAttribute('width', `${ceilToDecimal(width, 2)}`);
+        foreignObject.setAttribute('width', `${ceilToDecimal(width, 0)}`);
         foreignObject.setAttribute('height', `${height}`);
         foreignObject.setAttribute('x', `${x}`);
         foreignObject.setAttribute('y', `${y}`);
@@ -23,6 +23,6 @@ export function updateForeignObject(target: SVGForeignObjectElement | SVGGElemen
 export function updateForeignObjectWidth(target: SVGForeignObjectElement | SVGGElement, width: number) {
     const foreignObject = target instanceof SVGForeignObjectElement ? target : target.querySelector('foreignObject');
     if (foreignObject) {
-        foreignObject.setAttribute('width', `${ceilToDecimal(width, 2)}`);
+        foreignObject.setAttribute('width', `${ceilToDecimal(width, 0)}`);
     }
 }
